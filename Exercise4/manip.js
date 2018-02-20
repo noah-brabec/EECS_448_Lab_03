5d42860765c70473f199c78c73b1bb2d22a759ce
Noah Brabec <noah.brabec@gmail.com>
2018-02-20 14:20:39 -0600

    Completed exercise 4

--- Exercise4/manip.js
+++ Exercise4/manip.js
@@ -1,1 +1,20 @@
-j
+function changeBorder()
+{
+  var borderR = document.getElementById("borderR").value;
+  var borderG = document.getElementById("borderG").value;
+  var borderB = document.getElementById("borderB").value;
+  var borderW = document.getElementById("borderW").value;
+  var main = document.getElementById("main");
+
+  main.style.borderColor = "rgba("+borderR+","+borderG+","+borderB+","+borderW+")";
+}
+
+function changeBackground()
+{
+  var backgroundR = document.getElementById("backgroundR").value;
+  var backgroundG = document.getElementById("backgroundG").value;
+  var backgroundB = document.getElementById("backgroundB").value;
+  var main = document.getElementById("main");
+
+  main.style.backgroundColor = "rgb("+backgroundR+","+backgroundG+","+backgroundB+")";
+}
